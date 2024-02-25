4f2efc9e0fd45a15dbf88b4d36a8f7e8a86b0389
mariajosemackay <mariajoarevalon@gmail.com>
2024-02-24 20:47:21 -0700

    fixing it

---
+++ scripts/bodyrestaurants.js
@@ -0,0 +1,110 @@
+[
+    {
+      "restaurantsName": "Chick Fill A",
+      "Address": "1206 E 2100 S, Salt Lake City, Salt Lake City, Utah,USA",
+      "phone number": "801 486 0609",
+      "imageURL": "https://images.squarespace-cdn.com/content/v1/614e49f9f8c18318713c87d5/a13a50c2-eb5c-4727-8072-c1cd336be156/Chick-fil-A%C2%AE+Chicken+Sandwich+Meal+Facebook+Cover_master.jpg"
+    },
+    {
+      "restaurantsName": "Chipotle",
+      "Address": "1011 E 2100 S, Salt Lake City, Utah,United States",
+      "phone number": "801 467 1311",
+      "imageUrl": "https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=1200,height=672,format=auto/https://doordash-static.s3.amazonaws.com/media/store/header/a1488871-8669-47a6-8446-4e83b44296f3.jpg"
+    },
+    {
+      "restaurantsName": "Crack Shack",
+      "Address": "Manti,Salt Lake City, Utah, United States",
+      "phone number": "1888, May, 21",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+      "restaurantsName": "Costa Vida",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "2015, June, 7",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+      "restaurantsName": "KFC",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "2020, May, 2",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+      "restaurantsName": "Dees",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "2016, March, 20",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/provo-city-center/400x250/provo-city-center-temple-1572517-wallpaper.jpg"
+    },
+    {
+      "restaurantsName": "Olive Garden",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "1972, February, 9",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/provo-utah/400x250/provo-temple-lds-890642-wallpaper.jpg"
+    },
+    {
+      "restaurantsName": "Papa Johns",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "2001, April, 22",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/winter-quarters-nebraska/400x250/winter-quarters-nebraska-temple-detail-772766.jpg"
+    },
+    {
+      "restaurantsName": "Real Taqueria",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "1893, April, 6",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/salt-lake-city-utah/2018/400x250/slctemple5.jpg"
+    },
+    {
+      "restaurantsName": "Zupas",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "2019, September, 8",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/the-hague-netherlands/2019/400x250/3-hague-netherlands-temple-1088316.jpg"
+    },
+    {
+      "restaurantsName": "Dominos",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "1877, April, 6",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/st-george-utah/1280x800/st-george-temple-lds-149536-wallpaper.jpg"
+    },
+    {
+      "restaurantsName": "Washington D.C.",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "1974, November, 19",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+      "restaurantsName": "Jordan River Utah",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "1981, November, 16",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/jordan-river-utah/400x250/CWD_f5579d41-7344-4498-a589-4017ba5fcc91.jpg"
+    },
+    {
+      "restaurantsName": "Barranquilla Colombia",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "2018, December, 9",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/barranquilla-colombia/400x250/2-Barranquilla-Columblia-Temple-2135201.jpg"
+    },
+    {
+      "restaurantsName": "Lima Perú",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "1986, January, 10",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+      "restaurantsName": "Logan Utah",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "1884, May, 17",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/logan-utah/400x250/logan-temple-768119-wallpaper.jpg"
+    },
+    {
+      "restaurantsName": "Vernal Utah",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "1997, November, 2",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/vernal-utah/400x250/vernal-temple-lds-82531-wallpaper.jpg"
+    },
+    {
+      "restaurantsName": "Mexico City Mexico",
+      "Address": "Salt Lake City, Utah, United States",
+      "phone number": "1983, December, 2",
+      "imageUrl": "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    }
+  ]
